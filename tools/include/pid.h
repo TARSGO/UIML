3769f5974e8b003588a707b176023f3b952ac1d4
喜欢睡觉真君 <431433788@qq.com>
2023-08-24 13:48:19 +0000

    合并 !1 将pid改写为c++

--- tools/include/pid.h
+++ tools/include/pid.h
@@ -1,7 +1,9 @@
 #ifndef _USER_PID_H_
 #define _USER_PID_H_
 
-#include "config.h"
+#include "../../conf/config.h"
+
+#include <functional>
 
 #ifndef LIMIT
 #define LIMIT(x,min,max) (x)=(((x)<=(min))?(min):(((x)>=(max))?(max):(x)))
@@ -11,27 +13,33 @@
 #define ABS(x) ((x)>=0?(x):-(x))
 #endif
 
-typedef struct _PID
+class PID
 {
-	float kp,ki,kd;
-	float error,lastError;//误差、上次误差
-	float integral,maxIntegral;//积分、积分限幅
-	float output,maxOutput;//输出、输出限幅
-	float deadzone;//死区
-}PID;
+public:
+    float kp,ki,kd;
+    float error,lastError;//误差、上次误差
+    float integral,maxIntegral;//积分、积分限幅
+    float output,maxOutput;//输出、输出限幅
+    float deadzone;//死区
 
-typedef struct _CascadePID
+    PID(ConfItem* conf);
+    void SingleCalc(float reference,float feedback);
+    void FeedbackCalc(float reference, float feedback, const std::function<float(float)>& lambda);
+    void Clear();
+    void SetMaxOutput(float maxOutput);
+    void SetMaxIntegral(float maxIntegral);
+    void SetDeadzone(float deadzone);
+};
+
+class CascadePID
 {
-	PID inner;//内环
-	PID outer;//外环
-	float output;//串级输出，等于inner.output
-}CascadePID;
+public:
+    PID inner;//内环
+    PID outer;//外环
+    float output;//串级输出，等于inner.output
 
-void PID_Init(PID *pid, ConfItem* conf);
-void PID_SingleCalc(PID *pid,float reference,float feedback);
-void PID_CascadeCalc(CascadePID *pid,float angleRef,float angleFdb,float speedFdb);
-void PID_Clear(PID *pid);
-void PID_SetMaxOutput(PID *pid,float maxOut);
-void PID_SetDeadzone(PID *pid,float deadzone);
+    void CascadeCalc(float angleRef,float angleFdb,float speedFdb);
+};
+
 
 #endif
