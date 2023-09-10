cf9fb45f7f1785d7fbcf7db2b07960c85ff92fc5
RigoLigo <rigoligo03@gmail.com>
2023-09-10 21:10:41 +0800

    New: 合并梁凇赫的UART DMA实现

--- services/bsp/bsp_usart.c
+++ services/bsp/bsp_usart.c
@@ -41,39 +41,33 @@ UARTService uartService = {0};
 //函数声明
 void BSP_UART_Init(ConfItem* dict);
 void BSP_UART_InitInfo(UARTInfo* info, ConfItem* dict);
-void BSP_UART_Start_IT(UARTInfo* info);
 bool BSP_UART_BlockCallback(const char* name, SoftBusFrame* frame, void* bindData);
 bool BSP_UART_ItCallback(const char* name, SoftBusFrame* frame, void* bindData);
 bool BSP_UART_DMACallback(const char* name, SoftBusFrame* frame, void* bindData);
 void BSP_UART_InitRecvBuffer(UARTInfo* info);
+void BSP_UART_RecvConf(UARTInfo* info);
+void BSP_UART_RecvConf_DMA(UARTInfo* info, bool isDoubleBuffer);
+void BSP_UART_RecvHanlder(UARTInfo* uartInfo);
+void BSP_UART_RecvHanlder_DMA(UARTInfo* uartInfo);
 
 //uart接收中断回调函数
 void BSP_UART_IRQCallback(uint8_t huartX)
 {
 	UARTInfo* uartInfo = &uartService.uartList[huartX - 1];
 	
-	if(!uartService.initFinished)//如果初始化未完成则清除标志位
+	//如果初始化未完成则清除标志位
+	if(!uartService.initFinished)
 	{              
 		(void)uartInfo->huart->Instance->SR; 
 		(void)uartInfo->huart->Instance->DR;
 		return;
 	}
-	
-	if (__HAL_UART_GET_FLAG(uartInfo->huart, UART_FLAG_RXNE))
-	{
-		//防止数组越界
-		if(uartInfo->recvBuffer.pos < uartInfo->recvBuffer.maxBufSize)
-			uartInfo->recvBuffer.data[uartInfo->recvBuffer.pos++] = uartInfo->huart->Instance->DR;
-	}
-		
-	if (__HAL_UART_GET_FLAG(uartInfo->huart, UART_FLAG_IDLE))
-	{
-		/* clear idle it flag avoid idle interrupt all the time */
-		__HAL_UART_CLEAR_IDLEFLAG(uartInfo->huart);
-		uint16_t recSize=uartInfo->recvBuffer.pos; //此时pos值为一帧数据的长度
-		Bus_FastBroadcastSend(uartInfo->fastHandle, {{uartInfo->recvBuffer.data}, {.U16 = recSize}}); //空闲中断为一帧，发送一帧数据
-		uartInfo->recvBuffer.pos = 0;
-	}
+
+	//判断是否为DMA模式
+	if(!(uartInfo->huart->Instance->CR3 & USART_CR3_DMAR))
+		BSP_UART_RecvHanlder(uartInfo);
+	else	
+		BSP_UART_RecvHanlder_DMA(uartInfo);
 }
 
 //uart任务回调函数
@@ -128,20 +122,41 @@ void BSP_UART_InitInfo(UARTInfo* info, ConfItem* dict)
 
 	info->recvBuffer.maxBufSize = Conf_GetValue(dict, "max-recv-size", uint16_t, 1);
 	char name[] = "/uart_/recv";
-	name[5] = info[number-1].number + '0';
+	name[5] = info->number + '0';
 	info->fastHandle = Bus_CreateReceiverHandle(name);
 	//初始化接收缓冲区
-	BSP_UART_InitRecvBuffer(&info[number-1]);
-	//开启uart中断
-	BSP_UART_Start_IT(&info[number-1]);
+	BSP_UART_InitRecvBuffer(info);
+	//开启uart空闲中断
+	__HAL_UART_ENABLE_IT(info->huart, UART_IT_IDLE);
+
+	//判断是否为DMA接收
+	if(Conf_ItemExist(dict, "dma"))		//若为DMA接收，进入DMA配置函数
+		BSP_UART_RecvConf_DMA(info, Conf_GetValue(dict, "dma/double-buffer", bool, 0));
+	else								//若否，则使能RXNE中断
+		__HAL_UART_ENABLE_IT(info->huart, UART_IT_RXNE);
 }
 
-//开启uart中断
-void BSP_UART_Start_IT(UARTInfo* info)
+//uart DMA接收配置
+void BSP_UART_RecvConf_DMA(UARTInfo* info, bool isDoubleBuffer)
 {
-	__HAL_UART_ENABLE_IT(info->huart, UART_IT_RXNE);
-	__HAL_UART_ENABLE_IT(info->huart, UART_IT_IDLE);
+	//判断是否为双缓冲区模式
+	if(isDoubleBuffer)		//若为双缓冲区模式，则进行相应配置
+	{
+		//使能DMA串口接收
+		SET_BIT(info->huart->Instance->CR3, USART_CR3_DMAR);
+		//配置DMA双缓冲区
+		HAL_DMAEx_MultiBufferStart(info->huart->hdmarx, 
+									(uint32_t)(&info->huart->Instance->DR),
+									(uint32_t)(info->recvBuffer.data),
+									(uint32_t)(info->recvBuffer.data + info->recvBuffer.maxBufSize / 2),
+									info->recvBuffer.maxBufSize);
+	}
+	else
+	{
+		HAL_UART_Receive_DMA(info->huart, info->recvBuffer.data, info->recvBuffer.maxBufSize);
+	}
 }
+
 //初始化接收缓冲区
 void BSP_UART_InitRecvBuffer(UARTInfo* info)
 {
@@ -186,8 +201,83 @@ bool BSP_UART_DMACallback(const char* name, SoftBusFrame* frame, void* bindData)
 	uint8_t uartX = Bus_GetMapValue(frame, "uart-x").U8;
 	uint8_t* data = (uint8_t*)Bus_GetMapValue(frame, "data").Ptr;
 	uint16_t transSize = Bus_GetMapValue(frame, "trans-size").U16;
-	HAL_UART_Transmit_DMA(uartService.uartList[uartX - 1].huart,data,transSize);
+	HAL_UART_Transmit_DMA(uartService.uartList[uartX - 1].huart, data, transSize);
 	return true;
+}
+
+//非DMA模式下接收处理函数
+void BSP_UART_RecvHanlder(UARTInfo* uartInfo)
+{
+	//清除ORE标志位
+	if((__HAL_UART_GET_FLAG(uartInfo->huart, UART_FLAG_ORE) != RESET))
+	{
+		__HAL_UART_CLEAR_OREFLAG(uartInfo->huart);
+	}
+
+	//清除PE标志位
+	if((__HAL_UART_GET_FLAG(uartInfo->huart, UART_FLAG_PE) != RESET))
+	{
+		__HAL_UART_CLEAR_PEFLAG(uartInfo->huart);
+	}
+
+	//若为RXNE中断则接收数据
+	if (__HAL_UART_GET_FLAG(uartInfo->huart, UART_FLAG_RXNE))
+	{
+		//防止数组越界
+		if(uartInfo->recvBuffer.pos < uartInfo->recvBuffer.maxBufSize)
+			uartInfo->recvBuffer.data[uartInfo->recvBuffer.pos++] = uartInfo->huart->Instance->DR;
+	}
+
+	//若为IDLE中断则广播数据
+	if(__HAL_UART_GET_FLAG(uartInfo->huart, UART_FLAG_IDLE) != RESET)
+	{
+		__HAL_UART_CLEAR_IDLEFLAG(uartInfo->huart);
+		uint16_t recSize=uartInfo->recvBuffer.pos; //此时pos值为一帧数据的长度
+		Bus_FastBroadcastSend(uartInfo->fastHandle, {uartInfo->recvBuffer.data, &recSize}); //空闲中断为一帧，发送一帧数据
+		uartInfo->recvBuffer.pos = 0;
+	}
+}
+
+//DMA模式下接收处理函数
+void BSP_UART_RecvHanlder_DMA(UARTInfo* uartInfo)
+{
+	//判断是否为IDLE中断
+	if(__HAL_UART_GET_FLAG(uartInfo->huart, UART_FLAG_IDLE) != RESET)
+		__HAL_UART_CLEAR_IDLEFLAG(uartInfo->huart);
+	else
+		return;
+
+	//失能DMA
+	__HAL_DMA_DISABLE(uartInfo->huart->hdmarx);
+	//重新设定数据长度
+	__HAL_DMA_SET_COUNTER(uartInfo->huart->hdmarx, uartInfo->recvBuffer.maxBufSize);
+
+	//判断是否为双缓冲区
+	if(uartInfo->huart->hdmarx->Instance->CR & DMA_SxCR_DBM)
+	{
+		//切换缓冲区
+		uartInfo->huart->hdmarx->Instance->CR ^= DMA_SxCR_CT;
+		//使能DMA
+		__HAL_DMA_ENABLE(uartInfo->huart->hdmarx);
+		//判断当前缓冲区
+		if (uartInfo->huart->hdmarx->Instance->CR & DMA_SxCR_CT)
+		{
+			//广播接收到的数据
+			Bus_FastBroadcastSend(uartInfo->fastHandle, {uartInfo->recvBuffer.data});
+		}
+		else
+		{
+			//广播接收到的数据
+			Bus_FastBroadcastSend(uartInfo->fastHandle, {uartInfo->recvBuffer.data + uartInfo->recvBuffer.maxBufSize / 2});
+		}
+	}
+	else
+	{
+		//广播接收到的数据
+		Bus_FastBroadcastSend(uartInfo->fastHandle, {uartInfo->recvBuffer.data});
+		//使能DMA
+		__HAL_DMA_ENABLE(uartInfo->huart->hdmarx);
+	}
 }
 
 //生成中断服务函数
