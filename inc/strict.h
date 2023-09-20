3c714b8a96c51319ce38a2dcb43603296bddbae8
RigoLigo <rigoligo03@gmail.com>
2023-09-20 12:56:12 +0800

    New: 给strict模式失败断言暂时加入死循环，以后再加入急停

--- inc/strict.h
+++ inc/strict.h
@@ -3,7 +3,7 @@
 
 #ifdef UIML_STRICT_MODE
 
-#define UIML_CRASH_STRICT(reason)
+#define UIML_CRASH_STRICT(reason) while(1) {;} // TODO: PROPER EMERGENCY STOP AND ALARM!
 #define UIML_FATAL_ASSERT(condition,reason) if(!(condition)) { UIML_CRASH_STRICT(reason); }
 
 #else
