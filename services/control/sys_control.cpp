1432fa809a190ec534daed0c391b5fd5e7830060
RigoLigo <rigoligo03@gmail.com>
2023-11-06 17:39:28 +0800

    Change: 格式化代码

--- services/control/sys_control.cpp
+++ services/control/sys_control.cpp
@@ -1,8 +1,8 @@
 #include "cmsis_os.h"
-#include "softbus.h"
 #include "config.h"
-#include "pid.h"
 #include "main.h"
+#include "pid.h"
+#include "softbus.h"
 #include <string.h>
 
 typedef enum
@@ -10,92 +10,94 @@ typedef enum
     SYS_FOLLOW_MODE,
     SYS_SPIN_MODE,
     SYS_SEPARATE_MODE
-}SysCtrlMode;
+} SysCtrlMode;
 
 typedef struct
 {
     struct
     {
-        float vx,vy,vw;
-        float ax,ay;
-    } chassisData; //底盘数据
+        float vx, vy, vw;
+        float ax, ay;
+    } chassisData; // 底盘数据
 
     struct
     {
         float targetYaw, targetPitch; // 操纵云台欲达到的目标偏离角度
-        float relativeAngle; //云台偏离角度
-    } gimbalData; //云台数据
+        float relativeAngle;          // 云台偏离角度
+    } gimbalData;                     // 云台数据
 
     uint8_t mode;
     bool rockerCtrl; // 遥控器控制标志位
-    bool errFlag;  // 急停标志位
+    bool errFlag;    // 急停标志位
     PID rotatePID;
-}SysControl;
+} SysControl;
 
-SysControl sysCtrl={0};
+SysControl sysCtrl = {0};
 
-//函数声明
+// 函数声明
 void Sys_InitInfo(ConfItem *dict);
 void Sys_InitReceiver(void);
 void Sys_Broadcast(void);
 
-void Sys_Chassis_StopCallback(const char* name, SoftBusFrame* frame, void* bindData);
-void Sys_Chassis_MoveCallback(const char* name, SoftBusFrame* frame, void* bindData);
-void Sys_Mode_ChangeCallback(const char* name, SoftBusFrame* frame, void* bindData);
-void Sys_Gimbal_RotateCallback(const char* name, SoftBusFrame* frame, void* bindData);
-void Sys_Shoot_Callback(const char* name, SoftBusFrame* frame, void* bindData);
+void Sys_Chassis_StopCallback(const char *name, SoftBusFrame *frame, void *bindData);
+void Sys_Chassis_MoveCallback(const char *name, SoftBusFrame *frame, void *bindData);
+void Sys_Mode_ChangeCallback(const char *name, SoftBusFrame *frame, void *bindData);
+void Sys_Gimbal_RotateCallback(const char *name, SoftBusFrame *frame, void *bindData);
+void Sys_Shoot_Callback(const char *name, SoftBusFrame *frame, void *bindData);
 void Sys_ErrorHandle(void);
 
-//初始化控制信息
+// 初始化控制信息
 void Sys_InitInfo(ConfItem *dict)
 {
-    sysCtrl.mode = Conf_GetValue(dict, "init-mode", uint8_t, SYS_FOLLOW_MODE); //默认跟随模式
-    sysCtrl.rockerCtrl = Conf_GetValue(dict, "rocker-ctrl", bool, false);  //默认键鼠控制
+    sysCtrl.mode = Conf_GetValue(dict, "init-mode", uint8_t, SYS_FOLLOW_MODE); // 默认跟随模式
+    sysCtrl.rockerCtrl = Conf_GetValue(dict, "rocker-ctrl", bool, false);      // 默认键鼠控制
 
     sysCtrl.rotatePID.Init(Conf_GetNode(dict, "rotate-pid")); // 初始化小陀螺PID
 }
 
-//初始化接收
+// 初始化接收
 void Sys_InitReceiver()
 {
-    //底盘
-    Bus_SubscribeTopics(NULL, Sys_Chassis_MoveCallback, {"/rc/key/on-pressing","/rc/left-stick"});
+    // 底盘
+    Bus_SubscribeTopics(NULL, Sys_Chassis_MoveCallback, {"/rc/key/on-pressing", "/rc/left-stick"});
     Bus_SubscribeTopic(NULL, Sys_Chassis_StopCallback, "/rc/key/on-up");
-    //云台
-    Bus_SubscribeTopics(NULL, Sys_Gimbal_RotateCallback, {"/rc/mouse-move",
-                                                                "/rc/right-stick",
-                                                                "/gimbal/yaw/relative-angle"});	
-    //模式切换
-    Bus_SubscribeTopics(NULL, Sys_Mode_ChangeCallback, {"/rc/key/on-click","/rc/switch"});
-    //发射
-    Bus_SubscribeTopics(NULL, Sys_Shoot_Callback, {"/rc/key/on-click",
-                                                        "/rc/key/on-long-press",
-                                                        "/rc/key/on-up",
-                                                        "/rc/wheel"});
+    // 云台
+    Bus_SubscribeTopics(NULL,
+                        Sys_Gimbal_RotateCallback,
+                        {"/rc/mouse-move", "/rc/right-stick", "/gimbal/yaw/relative-angle"});
+    // 模式切换
+    Bus_SubscribeTopics(NULL, Sys_Mode_ChangeCallback, {"/rc/key/on-click", "/rc/switch"});
+    // 发射
+    Bus_SubscribeTopics(NULL,
+                        Sys_Shoot_Callback,
+                        {"/rc/key/on-click",
+                         "/rc/key/on-long-press",
+                         "/rc/key/on-up",
+                         "/rc/wheel"});
 }
 
-extern "C" void SYS_CTRL_TaskCallback(void const * argument)
+extern "C" void SYS_CTRL_TaskCallback(void const *argument)
 {
-    //进入临界区
+    // 进入临界区
     portENTER_CRITICAL();
     Sys_InitInfo((ConfItem *)argument);
     Sys_InitReceiver();
     portEXIT_CRITICAL();
-    while(1)
+    while (1)
     {
-        if(sysCtrl.errFlag==1)
+        if (sysCtrl.errFlag == 1)
             Sys_ErrorHandle();
 
-        if(sysCtrl.mode==SYS_FOLLOW_MODE)//跟随模式
+        if (sysCtrl.mode == SYS_FOLLOW_MODE) // 跟随模式
         {
             sysCtrl.rotatePID.SingleCalc(0, sysCtrl.gimbalData.relativeAngle);
             sysCtrl.chassisData.vw = sysCtrl.rotatePID.output;
         }
-        else if(sysCtrl.mode==SYS_SPIN_MODE)//小陀螺模式
+        else if (sysCtrl.mode == SYS_SPIN_MODE) // 小陀螺模式
         {
             sysCtrl.chassisData.vw = 240;
         }
-        else if(sysCtrl.mode==SYS_SEPARATE_MODE)// 分离模式
+        else if (sysCtrl.mode == SYS_SEPARATE_MODE) // 分离模式
         {
             sysCtrl.chassisData.vw = 0;
         }
@@ -104,197 +106,202 @@ extern "C" void SYS_CTRL_TaskCallback(void const * argument)
     }
 }
 
-//发送广播
+// 发送广播
 void Sys_Broadcast()
 {
-    Bus_RemoteFuncCall("/chassis/speed", {{"vx", {.F32 = sysCtrl.chassisData.vx}},
-                                      {"vy", {.F32 = sysCtrl.chassisData.vy}},
-                                      {"vw", {.F32 = sysCtrl.chassisData.vw}}});
-    Bus_RemoteFuncCall("/chassis/relative-angle", {{"angle", {.F32 = sysCtrl.gimbalData.relativeAngle}}});
-    Bus_RemoteFuncCall("/gimbal/setting", {{"yaw", {.F32 = sysCtrl.gimbalData.targetYaw}},
-                                       {"pitch", {.F32 = sysCtrl.gimbalData.targetPitch}}});
+    Bus_RemoteFuncCall("/chassis/speed",
+                       {{"vx", {.F32 = sysCtrl.chassisData.vx}},
+                        {"vy", {.F32 = sysCtrl.chassisData.vy}},
+                        {"vw", {.F32 = sysCtrl.chassisData.vw}}});
+    Bus_RemoteFuncCall("/chassis/relative-angle",
+                       {{"angle", {.F32 = sysCtrl.gimbalData.relativeAngle}}});
+    Bus_RemoteFuncCall("/gimbal/setting",
+                       {{"yaw", {.F32 = sysCtrl.gimbalData.targetYaw}},
+                        {"pitch", {.F32 = sysCtrl.gimbalData.targetPitch}}});
 }
 
-//底盘运动及停止回调函数
-void Sys_Chassis_MoveCallback(const char* name, SoftBusFrame* frame, void* bindData)
+// 底盘运动及停止回调函数
+void Sys_Chassis_MoveCallback(const char *name, SoftBusFrame *frame, void *bindData)
 {
-    float speedRatio=0;
-    if(!strcmp(name,"/rc/key/on-pressing") && !sysCtrl.rockerCtrl) //键鼠控制
+    float speedRatio = 0;
+    if (!strcmp(name, "/rc/key/on-pressing") && !sysCtrl.rockerCtrl) // 键鼠控制
     {
-        if(!Bus_CheckMapKeysExist(frame,{"combine-key","key"}))
+        if (!Bus_CheckMapKeysExist(frame, {"combine-key", "key"}))
             return;
-        if(!strcmp(Bus_GetMapValue(frame,"combine-key").Str, "none"))  //正常
-            speedRatio=1; 
-        else if(!strcmp(Bus_GetMapValue(frame,"combine-key").Str, "shift")) //快速
-            speedRatio=5; 
-        else if(!strcmp(Bus_GetMapValue(frame,"combine-key").Str, "ctrl")) //慢速
-            speedRatio=0.2;
-        switch(Bus_GetMapValue(frame,"key").Str[0])
+        if (!strcmp(Bus_GetMapValue(frame, "combine-key").Str, "none")) // 正常
+            speedRatio = 1;
+        else if (!strcmp(Bus_GetMapValue(frame, "combine-key").Str, "shift")) // 快速
+            speedRatio = 5;
+        else if (!strcmp(Bus_GetMapValue(frame, "combine-key").Str, "ctrl")) // 慢速
+            speedRatio = 0.2;
+        switch (Bus_GetMapValue(frame, "key").Str[0])
         {
-            case 'A': 
-                sysCtrl.chassisData.vx = 200*speedRatio;
-                break;
-            case 'D': 
-                sysCtrl.chassisData.vx = -200*speedRatio;
-                break;
-            case 'W': 
-                sysCtrl.chassisData.vy = 200*speedRatio;
-                break;
-            case 'S': 
-                sysCtrl.chassisData.vy = -200*speedRatio;
-                break;
+        case 'A':
+            sysCtrl.chassisData.vx = 200 * speedRatio;
+            break;
+        case 'D':
+            sysCtrl.chassisData.vx = -200 * speedRatio;
+            break;
+        case 'W':
+            sysCtrl.chassisData.vy = 200 * speedRatio;
+            break;
+        case 'S':
+            sysCtrl.chassisData.vy = -200 * speedRatio;
+            break;
         }
     }
-    else if(!strcmp(name,"/rc/left-stick") && sysCtrl.rockerCtrl) //遥控器控制
+    else if (!strcmp(name, "/rc/left-stick") && sysCtrl.rockerCtrl) // 遥控器控制
     {
-        if(!Bus_CheckMapKeysExist(frame,{"x","y"}))
+        if (!Bus_CheckMapKeysExist(frame, {"x", "y"}))
             return;
-        sysCtrl.chassisData.vx = Bus_GetMapValue(frame,"x").U16;
-        sysCtrl.chassisData.vy = Bus_GetMapValue(frame,"y").U16;
+        sysCtrl.chassisData.vx = Bus_GetMapValue(frame, "x").F32;
+        sysCtrl.chassisData.vy = Bus_GetMapValue(frame, "y").F32;
     }
 }
-void Sys_Chassis_StopCallback(const char* name, SoftBusFrame* frame, void* bindData)
+void Sys_Chassis_StopCallback(const char *name, SoftBusFrame *frame, void *bindData)
 {
 
-    if(sysCtrl.rockerCtrl || !Bus_CheckMapKeyExist(frame, "key"))
+    if (sysCtrl.rockerCtrl || !Bus_CheckMapKeyExist(frame, "key"))
         return;
-    switch(Bus_GetMapValue(frame,"key").U8)
+    switch (Bus_GetMapValue(frame, "key").U8)
     {
-        case 'A': 
-        case 'D': 
-            sysCtrl.chassisData.vx = 0;
-            break;
-        case 'W': 
-        case 'S': 
-            sysCtrl.chassisData.vy = 0;
-            break;
+    case 'A':
+    case 'D':
+        sysCtrl.chassisData.vx = 0;
+        break;
+    case 'W':
+    case 'S':
+        sysCtrl.chassisData.vy = 0;
+        break;
     }
 }
 
-//云台旋转回调函数
-void Sys_Gimbal_RotateCallback(const char* name, SoftBusFrame* frame, void* bindData)
+// 云台旋转回调函数
+void Sys_Gimbal_RotateCallback(const char *name, SoftBusFrame *frame, void *bindData)
 {
-    if(!strcmp(name,"/rc/mouse-move") && !sysCtrl.rockerCtrl)  //键鼠控制
+    if (!strcmp(name, "/rc/mouse-move") && !sysCtrl.rockerCtrl) // 键鼠控制
     {
-        if(!Bus_CheckMapKeysExist(frame,{"x","y"}))
+        if (!Bus_CheckMapKeysExist(frame, {"x", "y"}))
             return;
         // FIXME:鼠标能打多少量未知
         // sysCtrl.gimbalData.targetYaw +=*(int16_t*)Bus_GetMapValue(frame,"x");
-        // sysCtrl.gimbalData.targetPitch +=*(int16_t*)Bus_GetMapValue(frame,"y"); 
+        // sysCtrl.gimbalData.targetPitch +=*(int16_t*)Bus_GetMapValue(frame,"y");
     }
-    else if(!strcmp(name,"/rc/right-stick") && sysCtrl.rockerCtrl)  //遥控器控制
+    else if (!strcmp(name, "/rc/right-stick") && sysCtrl.rockerCtrl) // 遥控器控制
     {
-        if(!Bus_CheckMapKeysExist(frame,{"x","y"}))
+        if (!Bus_CheckMapKeysExist(frame, {"x", "y"}))
             return;
         // Yaw：【1 / 660 * 30 = 0.04545】（遥控器满杆量时，期望云台能转到偏移角30度位置）
         // Pitch：【1 / 660 * 10 = 0.01515】（满杆量时相对转动10度）//FIXME
-        sysCtrl.gimbalData.targetYaw = Bus_GetMapValue(frame,"x").U16 * 0.045;
-        sysCtrl.gimbalData.targetPitch = Bus_GetMapValue(frame,"y").U16 * 0.015; 
+        sysCtrl.gimbalData.targetYaw = Bus_GetMapValue(frame, "x").U16 * 0.045;
+        sysCtrl.gimbalData.targetPitch = Bus_GetMapValue(frame, "y").U16 * 0.015;
     }
-    else if(!strcmp(name,"/gimbal/yaw/relative-angle"))
+    else if (!strcmp(name, "/gimbal/yaw/relative-angle"))
     {
-        if(!Bus_CheckMapKeyExist(frame,"angle"))
+        if (!Bus_CheckMapKeyExist(frame, "angle"))
             return;
-        sysCtrl.gimbalData.relativeAngle = Bus_GetMapValue(frame,"angle").F32;
+        sysCtrl.gimbalData.relativeAngle = Bus_GetMapValue(frame, "angle").F32;
     }
 }
 
-//模式切换回调
-void Sys_Mode_ChangeCallback(const char* name, SoftBusFrame* frame, void* bindData)
+// 模式切换回调
+void Sys_Mode_ChangeCallback(const char *name, SoftBusFrame *frame, void *bindData)
 {
-    if(!strcmp(name,"/rc/key/on-click") && !sysCtrl.rockerCtrl)  //键鼠控制
+    if (!strcmp(name, "/rc/key/on-click") && !sysCtrl.rockerCtrl) // 键鼠控制
     {
-        if(!Bus_CheckMapKeyExist(frame,"key"))
+        if (!Bus_CheckMapKeyExist(frame, "key"))
             return;
-        switch(Bus_GetMapValue(frame,"key").U8)
+        switch (Bus_GetMapValue(frame, "key").U8)
         {
-            case 'Q':  
-                sysCtrl.mode = SYS_SPIN_MODE;  //小陀螺模式
-                break;
-            case 'E':  
-                sysCtrl.mode = SYS_FOLLOW_MODE;  //跟随模式
-                break;
-            case 'R':
-                sysCtrl.mode = SYS_SEPARATE_MODE; //分离模式
-                break;
+        case 'Q':
+            sysCtrl.mode = SYS_SPIN_MODE; // 小陀螺模式
+            break;
+        case 'E':
+            sysCtrl.mode = SYS_FOLLOW_MODE; // 跟随模式
+            break;
+        case 'R':
+            sysCtrl.mode = SYS_SEPARATE_MODE; // 分离模式
+            break;
         }
     }
-    else if(!strcmp(name,"/rc/switch") )  //遥控器控制
+    else if (!strcmp(name, "/rc/switch")) // 遥控器控制
     {
-        if(Bus_CheckMapKeyExist(frame, "right")&& sysCtrl.rockerCtrl)
+        if (Bus_CheckMapKeyExist(frame, "right") && sysCtrl.rockerCtrl)
         {
-            switch(Bus_GetMapValue(frame, "right").U8)
+            switch (Bus_GetMapValue(frame, "right").U8)
             {
-                case 1:
-                    sysCtrl.mode = SYS_SPIN_MODE; //小陀螺模式
-                    break;                        
-                case 3:                         
-                    sysCtrl.mode = SYS_FOLLOW_MODE;  //跟随模式
-                    break;                        
-                case 2:                         
-                    sysCtrl.mode = SYS_SEPARATE_MODE; //分离模式
-                    break;
-            }			
+            case 1:
+                sysCtrl.mode = SYS_SPIN_MODE; // 小陀螺模式
+                break;
+            case 3:
+                sysCtrl.mode = SYS_FOLLOW_MODE; // 跟随模式
+                break;
+            case 2:
+                sysCtrl.mode = SYS_SEPARATE_MODE; // 分离模式
+                break;
+            }
         }
-        if(Bus_CheckMapKeyExist(frame, "left"))
+        if (Bus_CheckMapKeyExist(frame, "left"))
         {
-            switch(Bus_GetMapValue(frame, "left").U8)
+            switch (Bus_GetMapValue(frame, "left").U8)
             {
-                case 1:
-                    sysCtrl.rockerCtrl = true; //切换至遥控器控制
-                    sysCtrl.errFlag = 0;
-                    break;
-                case 3:
-                    sysCtrl.rockerCtrl = false; //切换至键鼠控制
-                    sysCtrl.errFlag = 0;
-                    break;
-                case 2:   
-                    sysCtrl.errFlag = 1;
-                    break;
+            case 1:
+                sysCtrl.rockerCtrl = true; // 切换至遥控器控制
+                sysCtrl.errFlag = 0;
+                break;
+            case 3:
+                sysCtrl.rockerCtrl = false; // 切换至键鼠控制
+                sysCtrl.errFlag = 0;
+                break;
+            case 2:
+                sysCtrl.errFlag = 1;
+                break;
             }
         }
     }
 }
 
-//发射回调函数
-void Sys_Shoot_Callback(const char* name, SoftBusFrame* frame, void* bindData)
+// 发射回调函数
+void Sys_Shoot_Callback(const char *name, SoftBusFrame *frame, void *bindData)
 {
-    if(!strcmp(name,"/rc/key/on-click") && !sysCtrl.rockerCtrl)//键鼠控制
+    if (!strcmp(name, "/rc/key/on-click") && !sysCtrl.rockerCtrl) // 键鼠控制
     {
-        if(!Bus_CheckMapKeyExist(frame,"left"))
+        if (!Bus_CheckMapKeyExist(frame, "left"))
             return;
-        Bus_RemoteFuncCall("/shooter/mode",{{"mode", {.Str = "once"}}});  //点射
+        Bus_RemoteFuncCall("/shooter/mode", {{"mode", {.Str = "once"}}}); // 点射
     }
-    else if(!strcmp(name,"/rc/key/on-long-press") && !sysCtrl.rockerCtrl)
+    else if (!strcmp(name, "/rc/key/on-long-press") && !sysCtrl.rockerCtrl)
     {
-        if(!Bus_CheckMapKeyExist(frame,"left"))
+        if (!Bus_CheckMapKeyExist(frame, "left"))
             return;
-        Bus_RemoteFuncCall("/shooter/mode",{{"mode", {.Str = "continue"}}, {"interval-time", {.U32 = 100}}}); //连发
+        Bus_RemoteFuncCall("/shooter/mode",
+                           {{"mode", {.Str = "continue"}},
+                            {"interval-time", {.U32 = 100}}}); // 连发
     }
-    else if(!strcmp(name,"/rc/key/on-up") && !sysCtrl.rockerCtrl)
+    else if (!strcmp(name, "/rc/key/on-up") && !sysCtrl.rockerCtrl)
     {
-        if(!Bus_CheckMapKeyExist(frame,"left"))
+        if (!Bus_CheckMapKeyExist(frame, "left"))
             return;
-        Bus_RemoteFuncCall("/shooter/mode", {{"mode", {.Str = "idle"}}}); //连发
+        Bus_RemoteFuncCall("/shooter/mode", {{"mode", {.Str = "idle"}}}); // 连发
     }
-    else if(!strcmp(name,"/rc/wheel") && sysCtrl.rockerCtrl)//遥控器控制
+    else if (!strcmp(name, "/rc/wheel") && sysCtrl.rockerCtrl) // 遥控器控制
     {
-        if(!Bus_CheckMapKeyExist(frame,"value"))
+        if (!Bus_CheckMapKeyExist(frame, "value"))
             return;
         int16_t wheel = Bus_GetMapValue(frame, "value").I16;
 
-        if(wheel > 600)
-            Bus_RemoteFuncCall("/shooter/mode", {{"mode", {.Str = "once"}}}); //点射
+        if (wheel > 600)
+            Bus_RemoteFuncCall("/shooter/mode", {{"mode", {.Str = "once"}}}); // 点射
     }
 }
 
-//急停
+// 急停
 void Sys_ErrorHandle(void)
 {
     Bus_PublishTopic("/system/stop", {{"", {NULL}}});
-    while(1)
+    while (1)
     {
-        if(!sysCtrl.errFlag)
+        if (!sysCtrl.errFlag)
         {
             __disable_irq();
             NVIC_SystemReset();
--- tools/motor/motor_can/dji_motor.cpp
+++ tools/motor/motor_can/dji_motor.cpp
@@ -1,193 +1,194 @@
-#include "softbus.h"
+#include "config.h"
 #include "motor.h"
 #include "pid.h"
-#include "config.h"
+#include "softbus.h"
 #include "string.h"
 
-void DjiCanMotor::Init(ConfItem* dict)
+void DjiCanMotor::Init(ConfItem *dict)
 {
-	// 公共初始化部分
-	// 注意：必须设定好减速比与CAN信息
+    // 公共初始化部分
+    // 注意：必须设定好减速比与CAN信息
 
-	// 默认模式为扭矩模式
-	m_mode = MOTOR_TORQUE_MODE;
+    // 默认模式为扭矩模式
+    m_mode = MOTOR_TORQUE_MODE;
 
-	// 初始化电机pid
-	m_speedPid.Init(Conf_GetNode(dict, "speed-pid"));
-	m_anglePid.Init(Conf_GetNode(dict, "angle-pid"));
-	m_anglePid.outer.maxOutput *= m_reductionRatio; // 将输出轴速度限幅放大到转子上
+    // 初始化电机pid
+    m_speedPid.Init(Conf_GetNode(dict, "speed-pid"));
+    m_anglePid.Init(Conf_GetNode(dict, "angle-pid"));
+    m_anglePid.outer.maxOutput *= m_reductionRatio; // 将输出轴速度限幅放大到转子上
 
-	// 订阅can信息
-	char name[] = "/can_/recv";
-	name[4] = m_canInfo.canX + '0';
-	Bus_SubscribeTopic(this, DjiCanMotor::CanRxCallback, name);
+    // 订阅can信息
+    char name[] = "/can_/recv";
+    name[4] = m_canInfo.canX + '0';
+    Bus_SubscribeTopic(this, DjiCanMotor::CanRxCallback, name);
 
-	m_stallTime = 0;
+    m_stallTime = 0;
 }
 
 // 切换电机模式
 bool DjiCanMotor::SetMode(MotorCtrlMode mode)
 {
-	if (m_mode == MOTOR_STOP_MODE) // 急停模式下不允许切换模式
-		return false;
+    if (m_mode == MOTOR_STOP_MODE) // 急停模式下不允许切换模式
+        return false;
 
-	m_mode = mode;
-	switch (mode)
-	{
-	case MOTOR_SPEED_MODE:
-		m_speedPid.Clear();
-		break;
-	case MOTOR_ANGLE_MODE:
-		m_anglePid.Clear();
-		break;
-	default:
-		break;
-	}
+    m_mode = mode;
+    switch (mode)
+    {
+    case MOTOR_SPEED_MODE:
+        m_speedPid.Clear();
+        break;
+    case MOTOR_ANGLE_MODE:
+        m_anglePid.Clear();
+        break;
+    default:
+        break;
+    }
 
-	return true;
+    return true;
 }
 
 // 急停函数
 void DjiCanMotor::EmergencyStop()
 {
-	m_target = 0;
-	m_mode = MOTOR_STOP_MODE;
+    m_target = 0;
+    m_mode = MOTOR_STOP_MODE;
 }
 
 // 设置电机期望值
 bool DjiCanMotor::SetTarget(float target)
 {
-	switch (m_mode)
-	{
-	case MOTOR_SPEED_MODE:
-		m_target = target * m_reductionRatio;
-		break;
-	case MOTOR_ANGLE_MODE:
-		m_target = DegToCode(target, m_reductionRatio);
-		break;
-	case MOTOR_TORQUE_MODE:
-		m_target = target;
-		break;
-	default:
-		return false;
-	}
+    switch (m_mode)
+    {
+    case MOTOR_SPEED_MODE:
+        m_target = target * m_reductionRatio;
+        break;
+    case MOTOR_ANGLE_MODE:
+        m_target = DegToCode(target, m_reductionRatio);
+        break;
+    case MOTOR_TORQUE_MODE:
+        m_target = target;
+        break;
+    default:
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 // 开始统计电机累计角度
 bool DjiCanMotor::SetTotalAngle(float angle)
 {
-	m_totalAngle = DegToCode(angle, m_reductionRatio);
-	m_lastAngle = m_motorAngle;
+    m_totalAngle = DegToCode(angle, m_reductionRatio);
+    m_lastAngle = m_motorAngle;
 
-	return true;
+    return true;
 }
 
 // 获取电机数据
 float DjiCanMotor::GetData(MotorDataType type)
 {
-	switch (type)
-	{
-        case CurrentAngle:
-			// 转子当前角度
-			return CodeToDeg(m_motorAngle, 1);
+    switch (type)
+    {
+    case CurrentAngle:
+        // 转子当前角度
+        return CodeToDeg(m_motorAngle, 1);
 
-        case TotalAngle:
-			// 输出轴总转过角度
-			return CodeToDeg(m_totalAngle, m_reductionRatio);
+    case TotalAngle:
+        // 输出轴总转过角度
+        return CodeToDeg(m_totalAngle, m_reductionRatio);
 
-        case Speed:
-			// 输出轴转速（RPM）
-			return m_motorSpeed;
-		
-		default:
-			return 0;
-	}
+    case Speed:
+        // 输出轴转速（RPM）
+        return m_motorSpeed;
+
+    default:
+        return 0;
+    }
 }
 
-void DjiCanMotor::CanRxCallback(const char* endpoint, SoftBusFrame* frame, void* bindData)
+void DjiCanMotor::CanRxCallback(const char *endpoint, SoftBusFrame *frame, void *bindData)
 {
-	// 校验绑定电机对象
-	if (!bindData) return;
+    // 校验绑定电机对象
+    if (!bindData)
+        return;
 
-	auto self = reinterpret_cast<DjiCanMotor*>(bindData);
+    auto self = reinterpret_cast<DjiCanMotor *>(bindData);
 
-	// 校验电机ID
-	if (self->m_canInfo.rxID != Bus_GetListValue(frame, 0).U16)
-		return;
+    // 校验电机ID
+    if (self->m_canInfo.rxID != Bus_GetListValue(frame, 0).U16)
+        return;
 
-	auto data = reinterpret_cast<uint8_t*>(Bus_GetListValue(frame, 1).Ptr);
-	if (data)
-		self->CanRxUpdate(data); // 更新电机数据
+    auto data = reinterpret_cast<uint8_t *>(Bus_GetListValue(frame, 1).Ptr);
+    if (data)
+        self->CanRxUpdate(data); // 更新电机数据
 }
 
-void DjiCanMotor::TimerCallback(const void* argument)
+void DjiCanMotor::TimerCallback(const void *argument)
 {
-	auto self = reinterpret_cast<DjiCanMotor*>(pvTimerGetTimerID((TimerHandle_t)argument));
+    auto self = reinterpret_cast<DjiCanMotor *>(pvTimerGetTimerID((TimerHandle_t)argument));
 
-	// 计算前一帧至今转过的角度
-	self->UpdateAngle();
-	// 调用控制器计算电机下一步行动
-	self->ControllerUpdate(self->m_target);
+    // 计算前一帧至今转过的角度
+    self->UpdateAngle();
+    // 调用控制器计算电机下一步行动
+    self->ControllerUpdate(self->m_target);
 
-	// 判断堵转情况
+    // 判断堵转情况
 }
 
 // 电机急停回调函数
-void DjiCanMotor::EmergencyStopCallback(const char* endpoint, SoftBusFrame* frame, void* bindData)
+void DjiCanMotor::EmergencyStopCallback(const char *endpoint, SoftBusFrame *frame, void *bindData)
 {
-	DjiCanMotor* self = reinterpret_cast<DjiCanMotor*>(bindData);
-	self->m_target = 0;
-	self->m_mode = MOTOR_STOP_MODE;
+    DjiCanMotor *self = reinterpret_cast<DjiCanMotor *>(bindData);
+    self->m_target = 0;
+    self->m_mode = MOTOR_STOP_MODE;
 }
 
 // 统计电机累计转过的圈数
 void DjiCanMotor::UpdateAngle()
 {
-	int32_t angleDiff = 0;
-	// FIXME: 无注释，逻辑存疑
-	if (m_motorAngle - m_lastAngle < -4000) 
-		angleDiff = m_motorAngle + (8191 - m_lastAngle);
-	else if (m_motorAngle - m_lastAngle > 4000)
-		angleDiff = -m_lastAngle - (8191 - m_motorAngle);
-	else
-		angleDiff = m_motorAngle - m_lastAngle;
-	// 将角度增量加入计数器
-	m_totalAngle += angleDiff;
-	// 记录角度
-	m_lastAngle = m_motorAngle;
+    int32_t angleDiff = 0;
+    // FIXME: 无注释，逻辑存疑
+    if (m_motorAngle - m_lastAngle < -4000)
+        angleDiff = m_motorAngle + (8191 - m_lastAngle);
+    else if (m_motorAngle - m_lastAngle > 4000)
+        angleDiff = -m_lastAngle - (8191 - m_motorAngle);
+    else
+        angleDiff = m_motorAngle - m_lastAngle;
+    // 将角度增量加入计数器
+    m_totalAngle += angleDiff;
+    // 记录角度
+    m_lastAngle = m_motorAngle;
 }
 
 // 控制器根据模式计算输出
 void DjiCanMotor::ControllerUpdate(float target)
 {
-	int16_t output = 0;
-	switch (m_mode)
-	{
-	case MOTOR_SPEED_MODE:
-		output = m_speedPid.SingleCalc(target, m_motorSpeed);
-		break;
-	case MOTOR_ANGLE_MODE:
-		output = m_anglePid.CascadeCalc(target, m_totalAngle, m_motorSpeed);
-		break;
-	case MOTOR_TORQUE_MODE:
-		output = (int16_t) target;
-		break;
-	case MOTOR_STOP_MODE:
-		return;
-	}
+    int16_t output = 0;
+    switch (m_mode)
+    {
+    case MOTOR_SPEED_MODE:
+        output = m_speedPid.SingleCalc(target, m_motorSpeed);
+        break;
+    case MOTOR_ANGLE_MODE:
+        output = m_anglePid.CascadeCalc(target, m_totalAngle, m_motorSpeed);
+        break;
+    case MOTOR_TORQUE_MODE:
+        output = (int16_t)target;
+        break;
+    case MOTOR_STOP_MODE:
+        return;
+    }
 
-	CanTransmit(output);
+    CanTransmit(output);
 }
 
 // 堵转检测逻辑
 void DjiCanMotor::StallDetection()
 {
-	m_stallTime = (m_motorSpeed == 0 && abs(m_motorTorque) > 7000) ? m_stallTime + 2 : 0;
-	if (m_stallTime > 500)
-	{
-		Bus_PublishTopicFast(m_stallTopic, {{nullptr}});
-		m_stallTime = 0;
-	}
+    m_stallTime = (m_motorSpeed == 0 && abs(m_motorTorque) > 7000) ? m_stallTime + 2 : 0;
+    if (m_stallTime > 500)
+    {
+        Bus_PublishTopicFast(m_stallTopic, {{nullptr}});
+        m_stallTime = 0;
+    }
 }
