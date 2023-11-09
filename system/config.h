1261c6836c02fed0040ecc23e18fd3225b71bd97
RigoLigo <rigoligo03@gmail.com>
2023-11-09 11:20:02 +0800

    New: 修复C++ YAML配置获取API
    使用U_C(confptr)宏创建Conf对象

--- system/config.h
+++ system/config.h
@@ -1,94 +1,106 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-#include <stdint.h>
-#include <stdbool.h>
 #include "yamlparser.h"
+#include <stdbool.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-//弱符号，定义来自STM32 HAL库
-#if defined (__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050) /* ARM Compiler V6 */
-  #ifndef __weak
-    #define __weak  __attribute__((weak))
-  #endif
-  #ifndef __packed
-    #define __packed  __attribute__((packed))
-  #endif
-#elif defined ( __GNUC__ ) && !defined (__CC_ARM) /* GNU Compiler */
-  #ifndef __weak
-    #define __weak   __attribute__((weak))
-  #endif /* __weak */
-  #ifndef __packed
-    #define __packed __attribute__((__packed__))
-  #endif /* __packed */
+// 弱符号，定义来自STM32 HAL库
+#if defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050) /* ARM Compiler V6 */
+#ifndef __weak
+#define __weak __attribute__((weak))
+#endif
+#ifndef __packed
+#define __packed __attribute__((packed))
+#endif
+#elif defined(__GNUC__) && !defined(__CC_ARM) /* GNU Compiler */
+#ifndef __weak
+#define __weak __attribute__((weak))
+#endif /* __weak */
+#ifndef __packed
+#define __packed __attribute__((__packed__))
+#endif /* __packed */
 #endif /* __GNUC__ */
 
-//配置项类型
-// typedef struct {
-// 	char *name; //配置名
-// 	void *value; //配置值
-// } ConfItem;
+// 配置项类型
+//  typedef struct {
+//  	char *name; //配置名
+//  	void *value; //配置值
+//  } ConfItem;
 typedef const struct UimlYamlNode ConfItem;
 
 // 外设句柄表
-typedef struct {
-	const char *name;
-	void *handle;
+typedef struct
+{
+    const char *name;
+    void *handle;
 } PeriphHandle;
 
-//给用户配置文件用的宏封装
+// 给用户配置文件用的宏封装
 #define CF_DICT (ConfItem[])
-#define CF_DICT_END {NULL,NULL}
+#define CF_DICT_END                                                                                \
+    {                                                                                              \
+        NULL, NULL                                                                                 \
+    }
 
 #ifndef IM_PTR
-#define IM_PTR(type,...) (&(type){__VA_ARGS__}) //取立即数的地址
+#define IM_PTR(type, ...) (&(type){__VA_ARGS__}) // 取立即数的地址
 #endif
 
-//获取配置值，不应直接调用，应使用下方的封装宏
-ConfItem* _Conf_GetValue(ConfItem* dict, const char* name);
+// 获取配置值，不应直接调用，应使用下方的封装宏
+ConfItem *_Conf_GetValue(ConfItem *dict, const char *name);
 
 // 获取外设句柄，可以直接用但是提供了封装宏
-void* _Conf_GetPeriphHandle(const char* name);
+void *_Conf_GetPeriphHandle(const char *name);
 
 /*
-	@brief 判断字典中配置名是否存在
-	@param dict:目标字典
-	@param name:要查找的配置名，可通过'/'分隔以查找内层字典
-	@retval 0:不存在 1:存在
+    @brief 判断字典中配置名是否存在
+    @param dict:目标字典
+    @param name:要查找的配置名，可通过'/'分隔以查找内层字典
+    @retval 0:不存在 1:存在
 */
-#define Conf_ItemExist(dict,name) (_Conf_GetValue((dict),(name))!=NULL)
+#define Conf_ItemExist(dict, name) (_Conf_GetValue((dict), (name)) != NULL)
 
 /*
-	@brief 获取配置节点指针
-	@param dict:目标字典
-	@param name:要查找的配置名，可通过'/'分隔以查找内层字典
-	@retval 指向配置节点的ConfItem*型指针，若配置名不存在则返回NULL
+    @brief 获取配置节点指针
+    @param dict:目标字典
+    @param name:要查找的配置名，可通过'/'分隔以查找内层字典
+    @retval 指向配置节点的ConfItem*型指针，若配置名不存在则返回NULL
 */
-#define Conf_GetNode(dict,name) (_Conf_GetValue((dict),(name)))
+#define Conf_GetNode(dict, name) (_Conf_GetValue((dict), (name)))
 
 /*
-	@brief 获取配置值
-	@param dict:目标字典
-	@param name:要查找的配置名，可通过'/'分隔以查找内层字典
-	@param type:配置值的类型
-	@param def:默认值
-	@retval type型配置值数据，若配置名不存在则返回def
+    @brief 获取配置值
+    @param dict:目标字典
+    @param name:要查找的配置名，可通过'/'分隔以查找内层字典
+    @param type:配置值的类型
+    @param def:默认值
+    @retval type型配置值数据，若配置名不存在则返回def
 */
-#define Conf_GetValue(dict,name,type,def) (_Conf_GetValue((dict),(name))?(*(type*)(&(_Conf_GetValue((dict),(name))->U32))):(def))
+#define Conf_GetValue(dict, name, type, def)                                                       \
+    (_Conf_GetValue((dict), (name)) ? (*(type *)(&(_Conf_GetValue((dict), (name))->U32))) : (def))
 
 /*
-	@brief 获取外设句柄
-	@param name:外设名
-	@param type:外设句柄类型
-	@retval 外设句柄
+    @brief 获取外设句柄
+    @param name:外设名
+    @param type:外设句柄类型
+    @retval 外设句柄
 */
-#define Conf_GetPeriphHandle(name,type) ((type*)_Conf_GetPeriphHandle(name))
+#define Conf_GetPeriphHandle(name, type) ((type *)_Conf_GetPeriphHandle(name))
 
 #ifdef __cplusplus
 } // extern "C"
+#endif
+
+#ifdef __cplusplus
+
+#define U_C(confptr) UimlYamlNodeObject Conf(confptr)
+
 #endif
 
 #endif
--- system/yamlparser.h
+++ system/yamlparser.h
@@ -4,46 +4,62 @@
 #include <stddef.h>
 #include <stdint.h>
 
-struct UimlYamlNode {
+struct UimlYamlNode
+{
     uint32_t NameHash;
-    const char* NameRef;
-    struct UimlYamlNode* Next;
+    const char *NameRef;
+    struct UimlYamlNode *Next;
     union {
-        struct UimlYamlNode* Children;
+        struct UimlYamlNode *Children;
         uint32_t U32;
         int32_t I32;
         float F32;
-        char* Str;
-        void* Ptr;
+        char *Str;
+        void *Ptr;
     };
 };
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-size_t UimlYamlParse(const char* input, struct UimlYamlNode** output);
+    size_t UimlYamlParse(const char *input, struct UimlYamlNode **output);
 
-const struct UimlYamlNode* UimlYamlGetValue(const struct UimlYamlNode* input, const char* childName);
-const struct UimlYamlNode* UimlYamlGetValueByPath(const struct UimlYamlNode* input, const char* path);
+    const struct UimlYamlNode *UimlYamlGetValue(const struct UimlYamlNode *input,
+                                                const char *childName);
+    const struct UimlYamlNode *UimlYamlGetValueByPath(const struct UimlYamlNode *input,
+                                                      const char *path);
 
 #ifdef __cplusplus
 } // extern "C" {
 
-// class UimlYamlNodeObject
-// {
-// public:
-//     UimlYamlNodeObject() = delete;
-//     UimlYamlNodeObject(const UimlYamlNode* node) : m_node(node) {}
-    
-//     operator const UimlYamlNode*() const { return m_node; }
-    
-//     UimlYamlNodeObject operator[](const char* childName) const
-//     {
-//         return UimlYamlNodeObject(UimlYamlGetValue(m_node, childName));
-//     }
+class UimlYamlNodeObject
+{
+  private:
+    const UimlYamlNode *m_node;
 
-// private:
-//     const UimlYamlNode* m_node;
-// };
+  public:
+    UimlYamlNodeObject() = delete;
+    UimlYamlNodeObject(const UimlYamlNode *node) : m_node(node)
+    {
+    }
+
+    operator const UimlYamlNode *() const
+    {
+        return m_node;
+    }
+
+    UimlYamlNodeObject operator[](const char *childName) const
+    {
+        return UimlYamlNodeObject(UimlYamlGetValue(m_node, childName));
+    }
+
+    template <typename T> T get(T defval)
+    {
+        static_assert(sizeof(T) < sizeof(size_t));
+
+        return m_node ? *reinterpret_cast<T *>(&m_node->U32) : defval;
+    }
+};
 #endif
