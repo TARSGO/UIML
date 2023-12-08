29af5b9fa2436425044ebcce881f075ffa8855c7
RigoLigo <rigoligo03@gmail.com>
2023-12-08 17:45:41 +0800

    Fix: 传递va_list时应使用vsnprintf变种

--- tools/extendio/extendio.c
+++ tools/extendio/extendio.c
@@ -8,7 +8,7 @@ char *alloc_sprintf(void *(*alloc_method)(size_t), const char *format, ...)
     va_list args;
     va_start(args, format);
 
-    size_t length = snprintf(NULL, 0, format, args);
+    size_t length = vsnprintf(NULL, 0, format, args);
 
     char *ret = alloc_method(length + 1); // 补充结尾空字符
     if (ret == NULL)
